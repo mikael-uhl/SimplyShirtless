6a0a8b6c78d578569ac89cae5b61666f7c353995
brukob <mikaeluhl@gmail.com>
2023-11-19 21:08:00 -0300

    Further cleaning;

--- NoShirt/NoShirt.cs
+++ NoShirt/NoShirt.cs
@@ -4,48 +4,40 @@ using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewValley;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 namespace NoShirt
 {
     public class NoShirt
     {
-        private readonly IModHelper _helper;
         private readonly IMonitor _monitor;
         private static readonly Rectangle ShirtArea = new(8, 416, 8, 32);
         private static readonly Rectangle ShoulderArea = new(136, 416, 8, 32);
         
         private readonly Texture2D _blankRectangle;
         
-        private int _skinColor;
-
         public NoShirt(IModHelper helper, IMonitor monitor)
         {
-            _helper = helper;
             _monitor = monitor;
-            _blankRectangle = _helper.ModContent.Load<Texture2D>("assets/blank.png");
-            _helper.Events.Content.AssetRequested += this.RemoveShirt;
-            _helper.Events.Content.AssetRequested += this.RemoveShoulder;
+            _blankRectangle = helper.ModContent.Load<Texture2D>("assets/blank.png");
+            helper.Events.Content.AssetRequested += this.RemoveShirt;
+            helper.Events.Content.AssetRequested += this.RemoveShoulder;
         }
         
-        private bool IsAssetShirts(AssetRequestedEventArgs assetRequestEvent)
+        private static bool IsAssetShirts(AssetRequestedEventArgs assetRequestEvent)
         {
             return assetRequestEvent.NameWithoutLocale.IsEquivalentTo("Characters/Farmer/shirts");
         }
         
+        [SuppressMessage("ReSharper", "InconsistentNaming")]
         public static bool GetShirtData_Prefix(Farmer __instance, ref List<string> __result)
         {
-            if (__result == null)
-            {
-                __result = new List<string>();
-            }
+            __result ??= new List<string>();
 
-            if (__instance.shirt.Value == -1)
-            {
-                __result.Clear();
-                __result.Add("Sleeveless");
-                return false;
-            }
-            return true;
+            if (__instance.shirt.Value != -1) return true;
+            __result.Clear();
+            __result.Add("Sleeveless");
+            return false;
         }
         private void RemoveShirt(object sender, AssetRequestedEventArgs e)
         {
